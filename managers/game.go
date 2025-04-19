cdf2a5cfb287eb8ec665ea5eb5aa1ce6c0edae1e
mikabrytu <mikabrytu@gmail.com>
2025-04-19 06:59:34 -0300

    Listen to ai and player to control who can play

--- managers/game.go
+++ managers/game.go
@@ -1,24 +1,47 @@
 package managers
 
 import (
+	"fmt"
+	event_names "littlejumbo/genius/events"
 	"littlejumbo/genius/loopables"
 	"littlejumbo/genius/utils"
 
+	"github.com/mikabrytu/gomes-engine/events"
 	"github.com/mikabrytu/gomes-engine/render"
 )
 
-type Note struct {
-	id   int8
-	path string
-}
-
 var red, green, blue, yellow *loopables.Square
+var sequence []int
+var playerNoteCount int
 
 func Game() {
-	instantiateSquares()
+	prepareSquares()
+	prepareAI()
+
+	events.Subscribe(event_names.GENIUS_AI_SEQUENCE_FINISHED, func(params ...any) error {
+		sequence = params[0].([]interface{})[0].([]interface{})[0].([]int)
+		fmt.Printf("AI Sequence: %v\n", sequence)
+
+		enablePlayer(true)
+		return nil
+	})
+
+	events.Subscribe(event_names.GENIUS_PLAYER_SINGLE_NOTE_FINISHED, func(params ...any) error {
+		note := params[0].([]interface{})[0].([]interface{})[0].(int)
+		fmt.Printf("Player note: %d\n", note)
+
+		playerNoteCount++
+		if playerNoteCount == len(sequence) {
+			playerNoteCount = 0
+			enablePlayer(false)
+			loopables.EnablePlay(true)
+		}
+
+		return nil
+	})
 }
 
-func instantiateSquares() {
+func prepareSquares() {
 	var size int32 = 200
 	var offset int32 = 10
 	rect := render.RectSpecs{
@@ -38,8 +61,21 @@ func instantiateSquares() {
 	yrect.PosX += size + offset
 	yrect.PosY += size + offset
 
-	red = loopables.NewSquare("Red", rrect, render.Red, utils.NOTE_C_PATH)
-	green = loopables.NewSquare("Green", grect, render.Green, utils.NOTE_E_PATH)
-	blue = loopables.NewSquare("Blue", brect, render.Blue, utils.NOTE_G_PATH)
-	yellow = loopables.NewSquare("Yellow", yrect, render.Yellow, utils.NOTE_B_PATH)
+	red = loopables.NewSquare("Red", 0, rrect, render.Red, utils.NOTE_C_PATH)
+	green = loopables.NewSquare("Green", 1, grect, render.Green, utils.NOTE_E_PATH)
+	blue = loopables.NewSquare("Blue", 2, brect, render.Blue, utils.NOTE_G_PATH)
+	yellow = loopables.NewSquare("Yellow", 3, yrect, render.Yellow, utils.NOTE_B_PATH)
+}
+
+func prepareAI() {
+	loopables.NewAi()
+	loopables.EnablePlay(true)
+	loopables.LoadSquares([]*loopables.Square{red, green, blue, yellow})
+}
+
+func enablePlayer(enable bool) {
+	red.EnablePlay(enable)
+	green.EnablePlay(enable)
+	blue.EnablePlay(enable)
+	yellow.EnablePlay(enable)
 }
